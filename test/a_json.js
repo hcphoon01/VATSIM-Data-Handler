18035280a8b0d603fe7bba01980c0010598b7394
Harry Cameron <hcphoon01@gmail.com>
2021-03-13 13:53:33 +0000

    test: updated tests

--- test/a_json.js
+++ test/a_json.js
@@ -1,82 +1,89 @@
-const chai = require('chai');
-const chaiAsPromised = require('chai-as-promised');
-var chaiFiles = require('chai-files');
+const chai = require("chai");
+const chaiAsPromised = require("chai-as-promised");
+var chaiFiles = require("chai-files");
 chai.use(chaiFiles);
-chai.use(require('chai-json'));
+chai.use(require("chai-json"));
 chai.use(chaiAsPromised);
 chai.should();
 
-const FileHandler = require('../build/cjs/fileHandler');
-const expect = require('chai').expect;
-const path = require('path');
-const fs = require('fs');
+const FileHandler = require("../build/cjs/fileHandler");
+const expect = require("chai").expect;
+const path = require("path");
+const fs = require("fs");
 
 const fileHandler = new FileHandler.default();
-const json = path.basename('../vatsimData.json');
+const json = path.basename("../vatsimData.json");
 
-describe('#json handling', function () {
-	this.timeout(5000);
+describe("#json handling", function () {
+  this.timeout(5000);
 
-	before(() => {
-		return new Promise(async (resolve) => {
-			try {
-				fs.statSync(json);
-				resolve();
-			} catch (err) {
-				await fileHandler.initialUpdate();
-				resolve();
-			}
-		});
-	});
+  before(() => {
+    return new Promise(async (resolve) => {
+      try {
+        fs.statSync(json);
+        resolve();
+      } catch (err) {
+        await fileHandler.initialUpdate();
+        resolve();
+      }
+    });
+  });
 
-	it('should create vatsimData.json', async () => {
-		fileHandler.shouldUpdate().then(() => {
-			expect(json).to.be.a.jsonFile();
-		});
-	});
+  it("should create vatsimData.json", async () => {
+    fileHandler.shouldUpdate().then(() => {
+      expect(json).to.be.a.jsonFile();
+    });
+  });
 
-	it('should store the last update date in the json file', () => {
-		const file = fs.readFileSync(json);
-		const parsed = JSON.parse(file);
-		expect(new Date(parsed.updated_date)).to.be.below(new Date());
-	});
+  it("should store the last update date in the json file", () => {
+    const file = fs.readFileSync(json);
+    const parsed = JSON.parse(file);
+    expect(new Date(parsed.updated_date)).to.be.below(new Date());
+  });
 
-	it('should contain the clients, prefiles and servers', () => {
-		const file = fs.readFileSync(json);
-		const parsed = JSON.parse(file);
-		expect(parsed).to.include.all.keys('clients', 'prefiles', 'servers');
-	});
+  it("should contain the clients, prefiles and servers", () => {
+    const file = fs.readFileSync(json);
+    const parsed = JSON.parse(file);
+    expect(parsed).to.include.all.keys(
+      "pilots",
+      "controllers",
+      "prefiles",
+      "servers"
+    );
+  });
 
-	it('should update the json file', () => {
-		fs.createReadStream('vatsimData.json').pipe(fs.createWriteStream('oldVatsimData.json'));
-		fileHandler.update().then(() => {
-			expect(json).to.equal(file('oldVatsimData.json'))
-		})
-	});
+  it("should update the json file", () => {
+    fs.createReadStream("vatsimData.json").pipe(
+      fs.createWriteStream("oldVatsimData.json")
+    );
+    fileHandler.update().then(() => {
+      expect(json).to.equal(file("oldVatsimData.json"));
+    });
+  });
 
-	it('should check to see if the json file needs updating', () => {
-		expect(fileHandler.shouldUpdate()).to.eventually.equal(false);
-	});
+  it("should check to see if the json file needs updating", () => {
+    expect(fileHandler.shouldUpdate()).to.eventually.equal(false);
+  });
 
-	it('should get a different copy of the vatsim data file after 2 minutes', async function () {
-		this.timeout(140000);
-		await wait(130000);
-		fileHandler.update().then(() => {
-			expect(json).to.not.equal(file('oldVatsimData.json'));
-		});
-	});
+  it("should get a different copy of the vatsim data file after 2 minutes", async function () {
+    this.timeout(70000);
+    await wait(65000);
+    fileHandler.update().then(() => {
+      expect(json).to.not.equal(file("oldVatsimData.json"));
+    });
+  });
 
-	after(() => {
-		return new Promise(async (resolve) => {
-			try {
-				fs.unlinkSync('oldVatsimData.json')
-				resolve();
-			} catch (err) {
-				console.error(err);
-				resolve();
-			}
-		})
-	});
+  after(() => {
+    return new Promise(async (resolve) => {
+      try {
+        fs.unlinkSync("oldVatsimData.json");
+        resolve();
+      } catch (err) {
+        console.error(err);
+        resolve();
+      }
+    });
+  });
 
-	const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
+  const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 });
--- test/b_handler.js
+++ test/b_handler.js
@@ -1,197 +1,169 @@
-const {handler} = require('../build/cjs/methods');
-const chai = require('chai');
-const chaiAsPromised = require('chai-as-promised');
+const { handler } = require("../build/cjs/methods");
+const chai = require("chai");
+const chaiAsPromised = require("chai-as-promised");
 chai.use(chaiAsPromised);
 chai.should();
 
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const jsonFile = path.basename('../vatsimData.json');
+const jsonFile = path.basename("../vatsimData.json");
 
 const controller = {
-	"callsign": "EGLL_SUP",
-	"cid": "9999999",
-	"realname": "Test User",
-	"clienttype": "ATC",
-	"frequency": 199.998,
-	"latitude": -4.67434,
-	"longitude": 55.52184,
-	"altitude": 0,
-	"groundspeed": 0,
-	"planned_aircraft": null,
-	"planned_tascruise": null,
-	"planned_depairport": null,
-	"planned_altitude": null,
-	"planned_destairport": null,
-	"server": "UK-1",
-	"protrevision": 100,
-	"rating": 11,
-	"transponder": 0,
-	"facilitytype": 0,
-	"visualrange": 300,
-	"planned_revision": null,
-	"planned_flighttype": null,
-	"planned_deptime": null,
-	"planned_actdeptime": null,
-	"planned_hrsenroute": null,
-	"planned_minenroute": null,
-	"planned_hrsfuel": null,
-	"planned_minfuel": null,
-	"planned_altairport": null,
-	"planned_remarks": null,
-	"planned_route": null,
-	"planned_depairport_lat": 0,
-	"planned_depairport_lon": 0,
-	"planned_destairport_lat": 0,
-	"planned_destairport_lon": 0,
-	"atis_message": "Heathrow Supervisor",
-	"time_last_atis_received": "2020-04-06T00:54:36.0769604Z",
-	"time_logon": "2020-04-06T00:54:36.0769602Z",
-	"heading": 0,
-	"qnh_i_hg": 0,
-	"qnh_mb": 0
+  cid: 9999999,
+  name: "Test User",
+  callsign: "EGLL_SUP",
+  frequency: 199.998,
+  facility: 0,
+  rating: 11,
+  server: "UK-1",
+  visual_range: 300,
+  text_atis: ["Heathrow Supervisor", "This is the test"],
+  last_updated: "2020-04-06T00:54:36.0769604Z",
+  logon_time: "2020-04-06T00:54:36.0769602Z",
 };
 
 const pilot = {
-    "callsign": "VATSIMTEST1",
-    "cid": "1234567",
-    "realname": "TEST USER",
-    "clienttype": "PILOT",
-    "frequency": null,
-	"latitude": -4.67434,
-	"longitude": 55.52184,
-	"altitude": 100,
-	"groundspeed": 0,
-    "planned_aircraft": "A320/M",
-    "planned_tascruise": "480",
-    "planned_depairport": "EGLL",
-    "planned_altitude": "6000",
-    "planned_destairport": "EGLL",
-    "server": "UK-1",
-    "protrevision": 100,
-    "rating": 1,
-    "transponder": 1234,
-    "facilitytype": 0,
-    "visualrange": 0,
-    "planned_revision": "1",
-    "planned_flighttype": "I",
-    "planned_deptime": "1030",
-    "planned_actdeptime": "0",
-    "planned_hrsenroute": "1",
-    "planned_minenroute": "00",
-    "planned_hrsfuel": "4",
-    "planned_minfuel": "00",
-    "planned_altairport": "EGLL",
-    "planned_remarks": "/v/",
-    "planned_route": "DCT",
-    "planned_depairport_lat": 0,
-    "planned_depairport_lon": 0,
-    "planned_destairport_lat": 0,
-    "planned_destairport_lon": 0,
-    "atis_message": null,
-    "time_last_atis_received": "2020-04-05T14:00:38.8546725Z",
-    "time_logon": "2020-04-05T14:00:38.8546724Z",
-    "heading": 360,
-    "qnh_i_hg": 29.92,
-    "qnh_mb": 1013
+  cid: 1234567,
+  name: "TEST USER",
+  callsign: "VATSIMTEST1",
+  server: "UK-1",
+  pilot_rating: 1,
+  latitude: -4.67434,
+  longitude: 55.52184,
+  altitude: 100,
+  groundspeed: 0,
+  transponder: "1234",
+  heading: 360,
+  qnh_i_hg: 29.92,
+  qnh_mb: 1013,
+  flight_plan: {
+    flight_rules: "I",
+    aircraft: "A320/MA320/M-SDE2FGHIRWXY/LB1",
+    aircraft_faa: "A320/L",
+    aircraft_short: "A320",
+    departure: "EGLL",
+    arrival: "EGLL",
+    alternate: "EGLL",
+    cruise_tas: "480",
+    altitude: "6000",
+    deptime: "1030",
+    enroute_time: "0100",
+    fuel_time: "0400",
+    remarks: "/v/",
+    route: "DCT",
+  },
+  last_updated: "2020-04-05T14:00:38.8546725Z",
+  logon_time: "2020-04-05T14:00:38.8546724Z",
 };
 
-describe('#Data Handling', () => {
-	before(() => {
-		const file = fs.readFileSync(jsonFile);
-		const parsed = JSON.parse(file);
-		
-		parsed.clients.push(controller);
-		parsed.clients.push(pilot);
+describe("#Data Handling", () => {
+  before(() => {
+    const file = fs.readFileSync(jsonFile);
+    const parsed = JSON.parse(file);
 
-		const json = JSON.stringify(parsed);
+    parsed.controllers.push(controller);
+    parsed.pilots.push(pilot);
 
-		fs.writeFileSync('vatsimData.json', json);
-	});
+    const json = JSON.stringify(parsed);
 
-    describe(`getCount('all')`, () => {
-        it('should get all clients', () => {
-            (handler.getCount('all')).should.eventually.be.above(0);
-        });
+    fs.writeFileSync("vatsimData.json", json);
+  });
+
+  describe(`getCount('all')`, () => {
+    it("should get all clients", () => {
+      handler.getCount("all").should.eventually.be.above(0);
+    });
+  });
+
+  describe(`getCount('pilots')`, () => {
+    it("should get all pilots", () => {
+      handler.getCount("pilots").should.eventually.be.above(0);
+    });
+  });
+
+  describe(`getCount('controllers')`, () => {
+    it("should get all controllers", () => {
+      handler.getCount("controllers").should.eventually.be.above(0);
+    });
+  });
+
+  describe(`getCount('test')`, () => {
+    it("should return undefined when a random type is inputted", () => {
+      handler.getCount("test").should.eventually.be.undefined;
+    });
+  });
+
+  describe("getAirportInfo()", () => {
+    it("should get airport information for a given airport, EGLL", () => {
+      handler.getAirportInfo("EGLL").should.eventually.be.an("object").that.is
+        .not.empty;
     });
 
-    describe(`getCount('pilots')`, () => {
-        it('should get all pilots', () => {
-            (handler.getCount('pilots')).should.eventually.be.above(0);
-        });
+    it("should return an empty array when no airport is given", () => {
+      handler.getAirportInfo().should.eventually.be.undefined;
     });
 
-    describe(`getCount('controllers')`, () => {
-        it('should get all controllers', () => {
-            (handler.getCount('controllers')).should.eventually.be.above(0);
-        });
-	});
-	
-	describe(`getCount('test')`, () => {
-        it('should return undefined when a random type is inputted', () => {
-            (handler.getCount('test')).should.eventually.be.undefined;
-        });
+    it("should not include users with the frequency of 199.998", () => {
+      handler
+        .getAirportInfo("EGLL")
+        .should.eventually.be.an("object")
+        .that.does.not.include(controller);
     });
+  });
 
-    describe('getAirportInfo()', () => {
-        it('should get airport information for a given airport, EGLL', () => {
-            (handler.getAirportInfo('EGLL')).should.eventually.be.an('object').that.is.not.empty;
-        });
-
-        it('should return an empty array when no airport is given', () => {
-            (handler.getAirportInfo()).should.eventually.be.undefined;
-		});
-		
-		it('should not include users with the frequency of 199.998', () =>{
-			(handler.getAirportInfo('EGLL')).should.eventually.be.an('object').that.does.not.include(controller);
-		});
+  describe("getPopularAirports()", () => {
+    it("should get a list of the 10 most popular airports", () => {
+      handler
+        .getPopularAirports()
+        .should.eventually.be.an("array")
+        .to.have.lengthOf(10);
     });
+  });
 
-    describe('getPopularAirports()', () => {
-        it('should get a list of the 10 most popular airports', () => {
-            (handler.getPopularAirports()).should.eventually.be.an('array').to.have.lengthOf(10);
-        });
-	});
+  describe("getFlightInfo(callsign)", () => {
+    it("should get the flight infrmation for a connected callsign, VATSIMTEST1", () => {
+      handler.getFlightInfo("VATSIMTEST1").should.eventually.be.an("object")
+        .that.is.not.empty;
+    });
+    it("should return undefined for a non connected callsign", () => {
+      handler.getClientDetails("THISISAMADEUPCALLSIGN").should.eventually.be
+        .undefined;
+    });
+  });
 
-	describe('getFlightInfo(callsign)', () => {
-		it('should get the flight infrmation for a connected callsign, VATSIMTEST1', () => {
-			(handler.getFlightInfo('VATSIMTEST1')).should.eventually.be.an('object').that.is.not.empty;
-		});
-		it('should return undefined for a non connected callsign', () => {
-			(handler.getClientDetails('THISISAMADEUPCALLSIGN')).should.eventually.be.undefined;
-		});
-	});
+  describe("getClients()", () => {
+    it("should return a list of all clients", () => {
+      handler.getClients().should.eventually.be.an("array").that.is.not.empty;
+    });
+  });
 
-	describe('getClients()', () => {
-		it('should return a list of all clients', () => {
-			(handler.getClients()).should.eventually.be.an('array').that.is.not.empty;
-		});
-	});
-	
-	describe('getClientDetails(cid)', () => {
-		it('should get flight information for a connected CID, 1234567', () => {
-			(handler.getClientDetails(1234567)).should.eventually.be.an('object').that.is.not.empty;
-		});
-		it('should return undefined for a non connected CID', () => {
-			(handler.getClientDetails(999999)).should.eventually.be.undefined;
-		});
-	});
+  describe("getClientDetails(cid)", () => {
+    it("should get flight information for a connected CID, 1234567", () => {
+      handler.getClientDetails(1234567).should.eventually.be.an("object").that
+        .is.not.empty;
+    });
+    it("should return undefined for a non connected CID", () => {
+      handler.getClientDetails(999999).should.eventually.be.undefined;
+    });
+  });
 
-	describe('getSupervisors()', () => {
-		it('should return a list of supervisors connected to the VATSIM network, including a test supervisor', () => {
-			(handler.getSupervisors()).should.eventually.be.an('array').that.is.not.empty;
-		});
-	});
+  describe("getSupervisors()", () => {
+    it("should return a list of supervisors connected to the VATSIM network, including a test supervisor", () => {
+      handler.getSupervisors().should.eventually.be.an("array").that.is.not
+        .empty;
+    });
+  });
 
-	describe('getControllers()', () => {
-		it('should return a list of all controllers connected to the VATSIM network', () => {
-			(handler.getControllers()).should.eventually.be.an('array').that.is.not.empty;
-		});
-	});
+  describe("getControllers()", () => {
+    it("should return a list of all controllers connected to the VATSIM network", () => {
+      handler.getControllers().should.eventually.be.an("array").that.is.not
+        .empty;
+    });
+  });
 
-	after(() => {
-		fs.unlinkSync('vatsimData.json');
-		fs.unlinkSync('oldData.json');
-	});
+  after(() => {
+    fs.unlinkSync("vatsimData.json");
+    fs.unlinkSync("oldData.json");
+  });
 });
